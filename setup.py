bc9efb938ec38f5e2438164ae831a72504586c93
Will McCutchen <will@mccutch.org>
2014-05-02 12:09:32 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from distutils.core import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='dycco',
+    version='1.0.0',
+    description='Literate-programming-style documentation generator.',
+    long_description=read('README.rst'),
+    url='https://github.com/mccutchen/dycco',
+    license='MIT',
+    author='Will McCutchen',
+    author_email='will@mccutch.org',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=['dycco'],
+    package_data={
+        'dycco': ['resources/*'],
+    },
+    scripts=['bin/dycco'],
+    install_requires=read('requirements.txt').splitlines(),
+)
